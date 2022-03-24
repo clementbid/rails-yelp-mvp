4353fe719c287e2332d3f09bfc47f924542c22e9
bidard clement <bidard.clement.84@hotmail.com>
2022-03-24 11:02:35 +0100

    yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # get 'restaurants/index'
+  # get 'restaurants/new'
+  # get 'restaurants/create'
+  # get 'restaurants/show'
+  resources :restaurants
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
